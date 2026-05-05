607b69e893c41b59d0e2060be3a5cf61a57a5ae1
AbdelrhmanEzzat <64223277+AbdelrhmanEzzat@users.noreply.github.com>
2026-05-05 14:45:00 +0300

    test1

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
 //hi from abdo 11
+//test1
 module.exports = { add, subtract, multiply, divide };
